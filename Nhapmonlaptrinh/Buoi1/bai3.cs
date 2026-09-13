ad117b3ca5b36a8ea2c8eb2a7c9fe07c8f4a6329
daohuutai <taidaoxo@gmail.com>
2026-09-13 20:03:24 +0700

    Add files via upload

---
+++ Nhapmonlaptrinh/Buoi1/bai3.cs
@@ -0,0 +1,33 @@
+/*
+Một thiết bị hoạt động được h giờ, m phút và s giây. Hãy viết chương trình chuyển thời gian đó
+sang tổng số giây.
+*/
+
+using System;
+
+namespace NMLT.Buoi1
+{
+    class DoiSangGiay
+    {
+        public static void Main(string[] args)
+        {
+            int h, m, s, kq;
+
+            Console.Write("Nhap so gio: ");
+            h = int.Parse(Console.ReadLine());
+
+            Console.Write("Nhap so phut: ");
+            m = int.Parse(Console.ReadLine());
+
+            Console.Write("Nhap so giay: ");
+            s = int.Parse(Console.ReadLine());
+
+            kq = h * 3600 + m * 60 + s;
+
+            // In đúng định dạng bạn yêu cầu
+            Console.WriteLine($"Tong so giay cua {h}:{m}:{s} la {kq} giay");
+            
+            Console.Read();
+        }
+    }
+}
