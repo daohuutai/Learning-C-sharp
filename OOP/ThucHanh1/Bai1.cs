aa1fb2355d7002ae338449a89b37716fd5e49517
bjruly <bumjruly06@gmail.com>
2026-09-21 20:19:48 +0700

    Tuan03/OOP/Start

---
+++ OOP/ThucHanh1/Bai1.cs
@@ -0,0 +1,13 @@
+
+
+using System;
+
+public class Bai1
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Moi ban nhap ho va ten: ");
+        string hoTen = Console.ReadLine();
+        Console.WriteLine(hoTen);
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Nhapmon_C#
+## Thành viên nhóm
+- Trần Lê Gia Bảo - 3124411038
+- Đào Hữu Tài - 3124411261
+- Tô Thành Nhân - 3124411198
+- Nguyễn Thanh Tuấn - 3124411337
+- Huỳnh Nhật Quang - 3124411245
